18b36ea00ea7adc2a525385226644c9f62090f38
Asia <Asia@10.0.110.83>
2018-03-19 08:12:17 +0100

    init

---
+++ POLab4/src/main/java/pl/edu/ur/polab4/Main.java
@@ -0,0 +1,12 @@
+package pl.edu.ur.polab4;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO zad 4, 5
+    }
+    
+}
